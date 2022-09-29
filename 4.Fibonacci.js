d4435d41a360af54c6e9182ed323ab2f9a38508e
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-29 12:14:40 +0530

    Print the first n numbers in Fibonacci series

---
+++ 4.Fibonacci.js
@@ -0,0 +1,12 @@
+
+function fibonacci(n){
+    if(n < 2) return n
+    return fibonacci(n-1) + fibonacci(n-2)
+   }
+   
+   const num = 8;
+   
+   for(let i = 0; i<num;  ++i){
+    console.log(fibonacci(i))
+   }
+   
